967fddd0c995a7599f808667ae71875fd8f4c65e
Denzoll <elikhanovden@gmail.com>
2022-05-23 16:30:43 +0300

    deni

---
+++ index.js
@@ -0,0 +1,5 @@
+const figlet  =  require ( 'figlet' ) ;
+
+figlet ( ' Intocode' ,  function ( err ,  data )  { 
+    if  ( err )  { 
+        console.log ( ' Что - то пошло не так ... ' ) ; console.dir ( err ) ; return ; } console.log ( data ) } ) ;
